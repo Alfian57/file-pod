8e04d99ac215c535bad2ba384baaebfbb81be53e
Alfian57 <alfiangadingsaputra@gmail.com>
2025-12-25 22:32:36 +0700

    feat: preview on storage detail file list

--- lib/features/storage/presentation/widgets/storage_detail_file_list.dart
+++ lib/features/storage/presentation/widgets/storage_detail_file_list.dart
@@ -6,6 +6,7 @@ import 'package:file_pod/features/storage/presentation/widgets/dialogs/file_opti
 import 'package:file_pod/features/storage/presentation/widgets/dialogs/rename_dialog.dart';
 import 'package:file_pod/features/storage/presentation/widgets/dialogs/share_dialog.dart';
 import 'package:file_pod/features/storage/presentation/widgets/empty_states/empty_files_widget.dart';
+import 'package:file_pod/features/storage/presentation/widgets/file_preview_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:file_pod/features/storage/presentation/widgets/file_item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -116,6 +117,10 @@ class StorageDetailFileList extends ConsumerWidget {
               ref,
               storageDetail.files[i].id,
               storageDetail.files[i].originalName,
+            ),
+            onTap: () => FilePreviewHandler.showPreview(
+              context: context,
+              file: storageDetail.files[i],
             ),
           ),
           if (i < storageDetail.files.length - 1)
